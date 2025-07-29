e4eadb5b887149a98135c5dc44f8fb82c31955e7
ramakrishnamnit <ramakrishnamnit@gmail.com>
2025-07-29 14:21:37 +0530

    fix(lint): fix lint issues

--- eslint.config.js
+++ eslint.config.js
@@ -26,6 +26,7 @@ export default tseslint.config(
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "warn",
       "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
     },
   }
 );
